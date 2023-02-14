42633cfed023bb3c2595ed15b8f5a1f90d209c23
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-02-14 15:36:50 -0500

    feat: adding product and implement card component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,26 @@
+import Card from '../components/Card'
+import icon from '../assets/images/icon-cart.svg'
+import desktop from '../assets/images/image-product-desktop.jpg'
+
 const index = () => {
+  const product = [
+    {
+      icon: icon,
+      image: desktop,
+      subtitle: 'Perfume',
+      title: 'Gabrielle Essence Eau De Parfum',
+      description:
+        'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
+      price: '$149.99',
+      priceOld: '$169.99',
+    },
+  ]
   return (
-    <div className='w-full max-w-[1440px] m-auto p-4'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div
+      className='w-full max-w-[1440px] m-auto p-4 flex flex-col min-h-screen justify-center items-center'
+      role='main'
+    >
+      <Card data={product} />
     </div>
   )
 }
